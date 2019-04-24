fcc1477575a9793bb4725ccaf86c759645a8b11a
David McNee <info@circularone.co.uk>
2019-04-25 00:58:39 +0100

    Added saving of device token to rest resource

--- src/Plugin/rest/resource/RegisterDeviceTokenResource.php
+++ src/Plugin/rest/resource/RegisterDeviceTokenResource.php
@@ -2,13 +2,14 @@
 
 namespace Drupal\pushy\Plugin\rest\resource;
 
-use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\rest\ModifiedResourceResponse;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Provides a resource to get view modes by entity and bundle.
@@ -26,9 +27,16 @@ class RegisterDeviceTokenResource extends ResourceBase {
   /**
    * A current user instance.
    *
-   * @var \Drupal\Core\Session\AccountProxyInterface
+   * @var \Drupal\Core\Session\AccountInterface
    */
   protected $currentUser;
+
+  /**
+   * The curren request.
+   * 
+   * @var \Symfony\Component\HttpFoundation\Request
+   */
+  protected $request;
 
   /**
    * Constructs a new RegisterDeviceTokenResource object.
@@ -43,8 +51,10 @@ class RegisterDeviceTokenResource extends ResourceBase {
    *   The available serialization formats.
    * @param \Psr\Log\LoggerInterface $logger
    *   A logger instance.
-   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   * @param \Drupal\Core\Session\AccountInterface $current_user
    *   A current user instance.
+   * @param \Symfony\Component\HttpFoundation\Request
+   *   The request.
    */
   public function __construct(
     array $configuration,
@@ -52,10 +62,12 @@ class RegisterDeviceTokenResource extends ResourceBase {
     $plugin_definition,
     array $serializer_formats,
     LoggerInterface $logger,
-    AccountProxyInterface $current_user) {
+    AccountInterface $current_user,
+    Request $request) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
 
     $this->currentUser = $current_user;
+    $this->request = $request;
   }
 
   /**
@@ -68,15 +80,13 @@ class RegisterDeviceTokenResource extends ResourceBase {
       $plugin_definition,
       $container->getParameter('serializer.formats'),
       $container->get('logger.factory')->get('pushy'),
-      $container->get('current_user')
+      $container->get('current_user'),
+      $container->get('request_stack')->getCurrentRequest(),
     );
   }
 
   /**
    * Responds to PATCH requests.
-   *
-   * @param \Drupal\Core\Entity\EntityInterface $entity
-   *   The entity object.
    *
    * @return \Drupal\rest\ModifiedResourceResponse
    *   The HTTP response object.
@@ -84,15 +94,36 @@ class RegisterDeviceTokenResource extends ResourceBase {
    * @throws \Symfony\Component\HttpKernel\Exception\HttpException
    *   Throws exception expected.
    */
-  public function patch(EntityInterface $entity) {
+  public function patch($data) {
 
-    // You must to implement the logic of your REST Resource here.
-    // Use current user after pass authentication to validate access.
-    if (!$this->currentUser->hasPermission('access content')) {
+    if (!intval($this->currentUser->id())) {
       throw new AccessDeniedHttpException();
     }
 
-    return new ModifiedResourceResponse($entity, 204);
+    $device_type = $data['device'];
+    $token = $data['token'];
+    $device_type = $this->request->query->get('device');
+    $token = $this->request->query->get('token');
+
+    if (!$device_type || !$token) {
+      return new ModifiedResourceResponse([
+        'error' => t('Device and token must be provided'),
+      ], 406);
+    }
+
+    if (!in_array($device_type, ['expo', 'ios', 'android'])) {
+      return new ModifiedResourceResponse([
+        'error' => t('Device type must be one of expo, ios or android'),
+      ], 406);
+    }
+
+    $account = $this->currentUser->getAccount();
+    $tokens = $account->get('push_notification_device_token_' . $device_type)->value;
+    $tokens[] = ['value' => $token];
+    $account->set('push_notification_device_token_' . $device_type, $tokens);
+    $account->save();
+
+    return new ModifiedResourceResponse(['message' => t('Device token saved')], 200);
   }
 
 }
